9d92863e1b377c204f89218a47dff30102f84324
Francesco Abbate <francesco.bbt@gmail.com>
2024-08-10 11:35:04 +0200

    fix: just include SDL.h and avoid error on macOS
    On macOS filesystem is file insensitive so the include of the local file
    "sdl/sdl.h" from the project was conflicting with include <SDL/SDL.h> from
    the SDL library.
    
    To fix this problem and simplify we just include <SDL.h>. The pkg-config
    returns the directory SDL where SDL.h is located so that seems to be the
    right way to include SDL main header file.

--- game/player.c
+++ game/player.c
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "../video/opengl.h"
 
-#include "../sdl/sdl.h"
+#include <SDL.h>
 
 #include <stdio.h>
 #include <limits.h>
